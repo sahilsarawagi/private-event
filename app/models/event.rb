5f4ad0b7c1083ca93a91777b9fc6410c7a90ac23
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-02-11 13:35:40 +0530

    Implemented Event model class methods to separate past and upcoming events on the Event's Index page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: "attended_event_id", class_name: "EventAttendee"
   has_many :attendees, through: :event_attendances, source: :attendee
+
+  def future
+    event_date>= Date.current
+  end
+  def past
+    event_date< Date.current
+  end
 end
